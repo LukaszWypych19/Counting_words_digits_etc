5b64c09ceeba6f537c3fb3aac6733d06359f4a0b
Luk <lukasz.wypych19@gmail.com>
2023-04-04 09:24:34 +0200

    dodanie f liczenia liter i slow

--- liczenie_slow.py
+++ liczenie_slow.py
@@ -1,12 +1,30 @@
-# tekst = 'niespodzianka dupa dom kabaretdqwgeg'
-tekst = input('Napisz jakis wyraz lub tekst: ')
+# tekst = input('Napisz jakis wyraz lub tekst: ')
+
+plik = open('wyniki_f1.txt')
+czytaj = plik.read()
+print(czytaj)
+plik.close()
+
+
+
 def ilosc_znakow():
-    return len(tekst)
+    return len(czytaj)
 
 def ilosc_slow():
-    txt = len(tekst.split())
+    txt = len(czytaj.split())
     return txt
+
+def ilosc_liter():
+    slowa = 0
+
+    for char in czytaj:
+        char = char.lower()
+        if char == ' ':
+            slowa += 1
+        # else:
+
 
 print(f'W tym tekscie jest {ilosc_znakow()} znakow')
 print(f'W tym tekscie jest {ilosc_slow()} słow')
+print(f'W tym tekscie jest {ilosc_liter()} liter')
 
